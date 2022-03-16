65977bfcf4ebf95d7a762aa1fc9fdca54886592a
diptasahacse <diptasaha.lpu.cse@gmail.com>
2022-03-16 18:51:39 +0600

    load api and show some info on UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
@@ -13,12 +13,37 @@ function App() {
 
 function Country(){
   const [countries,setCountry] = useState([]);
-  console.log(countries)
+  
+
+  useEffect(()=>{
+    fetch('https://restcountries.com/v3.1/all')
+    .then(res=> res.json())
+    .then(data=>setCountry(data));
+  },[]);
+  
   return(
-    <div>
-      <h2>Name: Bangladesh</h2>
+    <div className='country-list'>
+      {
+        countries.map(country => <MakeCountryList singleCountry={country}></MakeCountryList>)
+
+      }
     </div>
   );
+}
+function MakeCountryList(props){
+  
+  const {area,population} = props.singleCountry;
+  const {png} = props.singleCountry.flags;
+  console.log(props.singleCountry)
+  return(
+    <div className='country'>
+      <p>Area: {area}</p>
+      <p>Population: {population}</p>
+      <img src={png} alt="" />
+
+    </div>
+  );
+
 }
 
 export default App;
