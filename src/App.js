42b708891f939be487b8c9bb784eabd786001583
diptasahacse <diptasaha.lpu.cse@gmail.com>
2022-03-16 18:14:28 +0600

    make a simple component and show on ui

--- src/App.js
+++ src/App.js
@@ -5,12 +5,20 @@ import { useState } from 'react';
 function App() {
   return (
     <div>
-      
+      <Country></Country>
       
     </div>
   );
 }
 
-
+function Country(){
+  const [countries,setCountry] = useState([]);
+  console.log(countries)
+  return(
+    <div>
+      <h2>Name: Bangladesh</h2>
+    </div>
+  );
+}
 
 export default App;
